037e1c00028c1f7491625da52b7fe351d5d645f8
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-07-14 22:00:58 +0530

    Complete the Project GameScout.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/logo.gif";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="100px" borderRadius={60} />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
